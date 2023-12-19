70368d8bb4569850ba9e363a2cb76304fc62e826
Janith Eranga <jeerange@gmail.com>
2023-12-19 14:50:52 +0530

    mobile optimized

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="flex h-[512px] md:h-[706px] relative font-inter">
-      <img src="/Hero.png" alt="Hero image" className="bg-no-repeat bg-cover lg:h-[763px] md:h-[448px] h-[218px]" />
-      <div className="bg-gradient-to-r to-[#1CBDDD] from-[#4DCA79] lg:w-[630px] absolute -bottom-6 md:bottom-1 lg:left-[80px] lg:top-[354px] flex flex-col px-[40px] py-[20px] lg:px-10 lg:py-5">
+    <section className="flex flex-col relative font-inter min-h-[512px]">
+      <img src="/Hero.png" alt="Hero image" className="w-full" />
+      <div className="bg-gradient-to-r to-[#1CBDDD] from-[#4DCA79] lg:w-[630px] lg:min-h-[306px] lg:absolute -bottom-6 md:bottom-14 md:left-[80px] flex flex-col px-[40px] py-[20px] lg:px-10 lg:py-5">
         <h1 className="font-bold text-[36px] md:text-[48px] md:leading-[1] leading-10 lg:text-5xl text-white">
           We crush your competitors, goals, and sales records - without the B.S.
         </h1>
